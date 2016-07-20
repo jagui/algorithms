710474fcb41eaaf57090a91e32760a1d6ccd51c3
Juan Agüí <juan.agui@telefonica.com>
2016-07-20 07:29:47 +0200

    implements a simpler recursive binary search

--- binarysearch.c
+++ binarysearch.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int binary_search(int value, int array[], int length){
+int binary_search_shrinking_array(int value, int array[], int length){
   if(length == 1)
       return array[0] == value;
 
@@ -21,13 +21,36 @@ int binary_search(int value, int array[], int length){
         half_length = 1;
     }
   }
-  return binary_search(value, array, half_length);
+  return binary_search_shrinking_array(value, array, half_length);
+}
+
+int binary_search(int value, int array[],int min_pos, int max_pos){
+  if(min_pos > max_pos)
+    return -1;
+
+  int mid_pos = (min_pos + max_pos)/2;
+  int guessed = array[mid_pos];
+  if(guessed == value)
+    return mid_pos;
+
+  if(guessed < value)
+  {
+    //Recurse over upper half
+    return binary_search(value, array, mid_pos+1, max_pos);
+  } else {
+    //Recurse over lower half
+    return binary_search(value, array, min_pos, mid_pos-1);
+  }
 }
 
 int main(int argc, char const *argv[]) {
-  /* code */
   int array[] = {0,1,2,3,4,5,6,7,8};
+
   for(int i=-2; i<11; i++){
-    printf("binary_search %d: found %d\n", i, binary_search(i, array,9));
+    printf("binary_search_shrinking_array %d: found %d\n", i, binary_search_shrinking_array(i, array,9));
+  }
+
+  for(int i=-2; i<11; i++){
+    printf("binary_search %d: found %d\n", i, binary_search(i, array,0 ,9));
   }
 }
